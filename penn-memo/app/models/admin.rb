579f54ea604565af3cb5bafd8b4dfde3d6719a5e
Wei-An <chocolate60333@vagvlan536.1565.wlan.wireless-pennnet.upenn.edu>
2016-11-05 15:07:13 -0400

    2.0 version

--- penn-memo/app/models/admin.rb
+++
@@ -1,7 +0,0 @@
-class Admin < ApplicationRecord
-	# attr_accessor :Author, :Title,:tag, :Content, :file
-
-	validates :Author, :presence => true
-	validates :Title, :presence => true
-	validates :Content, :presence => true
-end
