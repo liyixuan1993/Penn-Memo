d1a030fdb97fdcf738489af2449f68658592282f
Wei-An <chocolate60333@vagvlan536.1565.wlan.wireless-pennnet.upenn.edu>
2016-11-05 15:23:29 -0400

    chenge

--- penn-memo-1.0/app/views/memos/_memo.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! memo, :id, :title, :author, :date, :content, :priority, :created_at, :updated_at
-json.url memo_url(memo, format: :json)
--- penn-memo-1.0/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  resources :memos
-
-  root "memos#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
