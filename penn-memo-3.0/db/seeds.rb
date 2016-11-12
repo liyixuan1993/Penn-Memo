70ece61b91e82805d5ac3faf95948bbc3ac861e5
李旖轩 <liyixuan@vagvlan536.1518.wlan.wireless-pennnet.upenn.edu>
2016-11-12 14:13:07 -0500

    modify seed.rb

--- penn-memo-3.0/db/seeds.rb
+++ penn-memo-3.0/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Memo.destroy_all
+Category.destroy_all
+
+c1 = Category.create(:name => "1st Floor")
+c2 = Category.create(:name => "2nd Floor")
+c3 = Category.create(:name => "3rd Floor")
+
+Memo.create(:title => "Something new", :author => "Li", :date => "2016-11-12", :content => "Thanksgiving is coming!")
