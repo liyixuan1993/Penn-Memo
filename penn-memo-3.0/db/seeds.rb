b7b6c3179e2856b2bdf5413ffa85d5eceaf4a9cd
李旖轩 <liyixuan@vagvlan536.1518.wlan.wireless-pennnet.upenn.edu>
2016-11-12 14:50:40 -0500

    add something to seeds.db

--- penn-memo-3.0/db/seeds.rb
+++ penn-memo-3.0/db/seeds.rb
@@ -12,5 +12,10 @@ Category.destroy_all
 c1 = Category.create(:name => "1st Floor")
 c2 = Category.create(:name => "2nd Floor")
 c3 = Category.create(:name => "3rd Floor")
+c4 = Category.create(:name => "Accident and emergency (A&E)")
+c5 = Category.create(:name => "Diagnostic imaging")
+c6 = Category.create(:name => "Pharmacy")
 
-Memo.create(:title => "Something new", :author => "Li", :date => "2016-11-12", :content => "Thanksgiving is coming!")
+Memo.create(:title => "Something new", :author => "Li", :date => "2016-11-12", :category => c2, :content => "Thanksgiving is coming!", :priority=>"1")
+Memo.create(:title => "Something else", :author => "Chen", :date => "2016-11-13", :category => c1, :content => "Change medicine for patient 2.", :priority=>3)
+Memo.create(:title => "Buy medicine", :author => "Zhou", :date => "2016-11-13", :category => c6, :content => "Buy some painkiller!", :priority=>2)
