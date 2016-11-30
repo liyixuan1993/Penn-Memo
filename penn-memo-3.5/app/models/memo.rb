19b3fd2498e6a2321087ee0077ba5c645a953d87
hallelujarzhou <yazhou@seas.upenn.edu>
2016-11-30 01:37:31 -0500

    v3.5 UI modified

---
+++ penn-memo-3.5/app/models/memo.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: memos
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  author     :string
+#  date       :date
+#  content    :text
+#  priority   :boolean
+#  tag_id     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Memo < ApplicationRecord
+  belongs_to :tag
+  has_many :responses
+  has_many :memos, through: :responses
+  has_attached_file :image
+  validates_attachment :image, content_type: { content_type: ["application/pdf", "image/jpeg", "image/gif", "image/png"] }
+              
+end
