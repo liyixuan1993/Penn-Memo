3478df6f297a0ba604e32ebf8ec6c4b97e8c0180
hallelujarzhou <yazhou@seas.upenn.edu>
2016-12-03 23:48:36 -0500

    ui revise

---
+++ penn-memo-3.7/app/controllers/memos_controller.rb
@@ -0,0 +1,93 @@
+class MemosController < ApplicationController
+  before_action :set_memo, only: [:show, :edit, :update, :destroy]
+
+  # GET /memos
+  # GET /memos.json
+  def index
+    @memos = Memo.all
+    @tags = Tag.all
+    if params[:search]
+      @memos = Memo.search(params[:search]).order("created_at DESC")
+    else
+      @memos = Memo.all
+    end
+  end
+
+  # GET /memos/1
+  # GET /memos/1.json
+  def show
+    @memo = Memo.find(params[:id])
+    @response = Response.new(:memo => @memo)
+  end
+
+  # GET /memos/new
+  def new
+    @memo = Memo.new
+  end
+
+  def memolist
+    @memos = Memo.all
+  end
+
+  # GET /memos/1/edit
+  def edit
+  end
+
+  # POST /memos
+  # POST /memos.json
+  def create
+    @memo = Memo.new(memo_params)
+
+    respond_to do |format|
+      if @memo.save
+        format.html { redirect_to @memo, notice: 'Memo was successfully created.' }
+        format.json { render :show, status: :created, location: @memo }
+      else
+        format.html { render :new }
+        format.json { render json: @memo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /memos/1
+  # PATCH/PUT /memos/1.json
+  def update
+    respond_to do |format|
+      if @memo.update(memo_params)
+        format.html { redirect_to @memo, notice: 'Memo was successfully updated.' }
+        format.json { render :show, status: :ok, location: @memo }
+      else
+        format.html { render :edit }
+        format.json { render json: @memo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /memos/1
+  # DELETE /memos/1.json
+  def destroy
+    @memo.destroy
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Memo was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+
+
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_memo
+      @memo = Memo.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def memo_params
+      params.require(:memo).permit(:title, :author, :date, :content, :image, :priority, :tag_id)
+    end
+
+end
